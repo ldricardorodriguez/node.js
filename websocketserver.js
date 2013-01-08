90c6b8e0cc07110f9b2f42eeb19b9490973751d0
Ricardo Rodriguez Lorenzo <ricardo.rodriguez@lineadatascan.com>
2013-01-08 17:02:34 -0430

    Con redimensionamiento automatico de las imagenes.
    Es necesario instalar GraphicsMagick
    (http://www.graphicsmagick.org/index.html)

--- websocketserver.js
+++ websocketserver.js
@@ -46,6 +46,15 @@ server.sockets.on("connection", function(message)
 			
 			for (var i in files) {
 				var currentFile = fullpath + '/' + files[i];
+				
+				var gm = require('gm');
+				gm(currentFile)
+				.resize(200, 110)
+				.write(currentFile, function (err) {
+				  if (!err) console.log(' hooray! ');
+				  if (err) throw err;
+				});
+				
 				var stats = fs.statSync(currentFile);
 				if (stats.isFile()) { //Se comprueba que sean archivos, así no se envía un url en caso de que sea un directorio.
 					console.log(currentFile);
